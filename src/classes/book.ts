fdb388030d7a1dbea6892105b97d777e30640244
Rui Andrade <ruiandrade@outlook.com>
2019-07-31 23:31:54 +0100

    gitignore correction

---
+++ src/classes/book.ts
@@ -0,0 +1,68 @@
+import cheerio from 'cheerio';
+import { Summary } from './summary';
+
+export class Book {
+    public title: string;
+    public description: string;
+    public thumbnail: URL | undefined;
+    public provider : string;
+    public summary : Summary;
+    public price : number | undefined;
+    public isbn : string | undefined; // Subject to change
+
+    constructor(html: string, provider: string, summary : Summary) {
+        this.provider = provider;
+        this.summary = summary;
+        
+        const $ = cheerio.load(html);
+
+        this.title = $("h1").text();
+        this.description = $(".strate > .whiteContent > p").text();
+
+        let findImgNode = $("img.f-productVisuals-mainMedia");
+        if (findImgNode.length == 0 || findImgNode.get(0).attribs.src.startsWith("data:image")) {
+            this.thumbnail = undefined;
+        }
+        else {
+            let potentialNode = findImgNode.get(0);
+            try {
+                this.thumbnail = new URL(potentialNode.attribs.src);
+            }
+            catch (e) {
+                try {
+                    this.thumbnail = new URL(potentialNode.attribs["data-lazyimage"]);
+                }
+                catch (e) {
+                    this.thumbnail = undefined;
+                }
+            }
+        }
+
+        let findPriceNode = $(".f-productOffers-tabs .js-productOffersTab");
+        let priceClass = "";
+        if (findPriceNode.length == 0) {
+            if ($(".f-shopBox-MPInfoSellerItem f-shopBox-MPInfoSellerTitle").length == 0) {
+                priceClass = ".f-priceBox-price";
+            }
+        }
+        else {
+            priceClass = "f-productOffers-tabLabel--price";
+        }
+
+        if (priceClass != "") {
+            this.price = parseFloat($(priceClass).text().replace("€", "").replace(",", "."));
+        }
+
+        let featureNodes = $(".Feature-item");
+        featureNodes.each((index: number, element: CheerioElement) => {
+            let $$ = cheerio.load(element);
+            if ($$(".Feature-label").text().trim().toUpperCase() == "ISBN") {
+                this.isbn = $$(".Feature-desc").text().trim().replace("-", "").replace("/", "");
+            }
+        });
+    }
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
---
+++ src/classes/summary.ts
@@ -0,0 +1,51 @@
+import cheerio from 'cheerio';
+import axios, { AxiosResponse } from 'axios';
+import { Book } from './book';
+
+export class Summary {
+    public title: string;
+    public description: string;
+    public url: URL;
+    public thumbnail: URL | undefined;
+    public provider : string;
+    public book : Promise<Book>;
+
+    constructor(html: string, provider: string) {
+        this.provider = provider;
+
+        const $ = cheerio.load(html);
+
+        this.title = $(".Article-desc > a").text();
+        this.description = $(".moreInfos-summary > p > span").text();
+        this.url = new URL($("p.Article-desc > a").get(0).attribs.href, "https://www.fnac.pt/");
+
+        let findImgNode = $("img.Article-itemVisualImg");
+        if (findImgNode.length == 0 || findImgNode.get(0).attribs.src.startsWith("data:image")) {
+            this.thumbnail = undefined;
+        }
+        else {
+            let potentialNode = findImgNode.get(0);
+            try {
+                this.thumbnail = new URL(potentialNode.attribs.src);
+            }
+            catch (e) {
+                try {
+                    this.thumbnail = new URL(potentialNode.attribs["data-lazyimage"]);
+                }
+                catch (e) {
+                    this.thumbnail = undefined;
+                }
+            }
+        }
+
+        this.book = new Promise((resolve, reject) => {
+            axios.get(this.url.href).then((response : AxiosResponse) => {
+                resolve(new Book(response.data, this.provider, this));
+            });
+        });
+    }
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
